84e5acf5852f4c021a24dbc1d3b12403f1099563
Luis <luisprooc@gmail.com>
2021-02-09 12:08:17 -0400

    Phrases added in the DB

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from random import randint
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/phrase')
+def random_phrase():
+    
+    algo = randint(1,35)
+
+    prueba = {"mensaje":algo}
+    return jsonify(prueba)
 
 
 if __name__ == '__main__':
