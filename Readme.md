9e0a36a74080e74bf03005ff075c6b500867966e
Luis <luisprooc@gmail.com>
2021-02-09 18:04:57 -0400

    Random phrase generate and project finished

--- Readme.md
+++ Readme.md
@@ -1,2 +1,7 @@
 # RANDOM PHRASE GENERATOR
 
+*Read the requirements*
+
+![ScreenShot](screenShot.PNG)
+
+The API generate a random phrase when you visit route /phrase
--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import Flask, jsonify
 from random import randint
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/phrase')
 def random_phrase():
-    
-    algo = randint(1,35)
 
-    prueba = {"mensaje":algo}
-    return jsonify(prueba)
+    try:
+        # connect at the DB and get phrase
+        conn = sqlite3.connect('DB/phrases.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT ID, AUTOR, PHRASE FROM ALL_PHRASES WHERE ID = {0}".format(randint(1,35)))
+        req = cursor.fetchone()
+
+        # convert phrase in object
+        res = {
+            "ID":req[0],
+            "Author":req[1],
+            "Phrase":req[2]
+        }
+
+        conn.commit()
+        conn.close()
+        
+        # Return response
+        return jsonify(res)
+    
+    except:
+        return jsonify("PHRASE NOT FOUND")
+    
 
 
 if __name__ == '__main__':
